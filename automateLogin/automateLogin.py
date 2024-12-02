a9dffcfd062dafa4d79de21fbd4c3620ee59c564
deepak94 <deepak.m@skava.com>
2024-12-02 16:39:37 +0530

    updated

--- automateLogin/automateLogin.py
+++ automateLogin/automateLogin.py
@@ -33,6 +33,7 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")  # If running on a server
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
 
 # # Launch Chrome browser
 driver = webdriver.Chrome(service=service, options=options)
